5500a73a263358c2a26f2cc9e3aa148e353c6df5
Jeff Jacobson <jacobsj@wsdot.wa.gov>
2012-02-21 16:46:51 -0800

    Added metadataList and validLayers resources.

--- LayerMetadataSoe/LayerMetadataSoe.cs
+++ LayerMetadataSoe/LayerMetadataSoe.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Web.Script.Serialization;
 using System.Xml;
 using System.Xml.Xsl;
 using ESRI.ArcGIS.Carto;
@@ -48,6 +49,10 @@ namespace LayerMetadataSoe
 		{
 			RestResource soeResource = new RestResource("LayerMetadataSoe", false, RootSOE);
 
+			RestResource metadataListResource = new RestResource("metadataList", true, GetMetadataList);
+
+			RestResource validLayersResource = new RestResource("validLayers", false, GetIdsOfLayersThatHaveMetadata);
+
 			RestOperation getLayerMetadataOp = new RestOperation("getLayerMetadata",
 				new string[] { "layer" },
 				new string[] { "xml", "html" },
@@ -55,6 +60,9 @@ namespace LayerMetadataSoe
 				);
 
 			soeResource.operations.Add(getLayerMetadataOp);
+
+			soeResource.resources.Add(validLayersResource);
+			soeResource.resources.Add(metadataListResource);
 			
 			return soeResource;
 		}
@@ -85,6 +93,30 @@ namespace LayerMetadataSoe
 			return Encoding.UTF8.GetBytes(jObject.ToJson());
 		}
 
+		//metadataList/{metadataListID}
+		//returns json with simplified layerinfo (name, id, extent)
+		private byte[] GetMetadataList(NameValueCollection boundVariables, string outputFormat, string requestProperties, out string responseProperties)
+		{
+			responseProperties = null;
+
+			//layerID
+			int layerID = Convert.ToInt32(boundVariables["metadataListID"]);
+
+			//execute
+			var xml = GetMetadataXml(ref responseProperties, layerID);
+
+			if (Regex.IsMatch(outputFormat, @"(?i)html?"))
+			{
+				// Transform to HTML using XSLT.
+				responseProperties = "{\"Content-Type\" : \"text/html\"}";
+				return TransformToHtml(xml);
+			}
+			else
+			{
+				return Encoding.UTF8.GetBytes(xml);
+			}
+		}
+
 		private byte[] GetMetadataForLayer(NameValueCollection boundVariables,
 			JsonObject operationInput,
 			string outputFormat,
@@ -95,6 +127,7 @@ namespace LayerMetadataSoe
 
 			long? layerId;
 			
+
 			// Throw an exception if a layer ID value was not provided.
 			if (!operationInput.TryGetAsLong("layer", out layerId))
 			{
@@ -102,40 +135,7 @@ namespace LayerMetadataSoe
 			}
 			
 		    int layerIdInt = (int)layerId.Value;
-			var mapServer = (IMapServer3)_serverObjectHelper.ServerObject;
-			string defaultMapName = mapServer.DefaultMapName;
-
-
-
-			var msDataAccess = (IMapServerDataAccess)_serverObjectHelper.ServerObject;
-			IDataset layer = null;
-
-			string xml = null;
-			responseProperties = "{\"Content-Type\" : \"text/xml\"}";
-			try
-			{
-				layer = msDataAccess.GetDataSource(defaultMapName, layerIdInt) as IDataset;
-				if (layer != null)
-				{
-					IMetadata metadata = layer.FullName as IMetadata;
-					if (metadata != null)
-					{
-						IXmlPropertySet2 propSet = metadata.Metadata as IXmlPropertySet2;
-						if (propSet != null)
-						{
-							xml = propSet.GetXml(string.Empty);
-						}
-					}
-				}
-				if (string.IsNullOrEmpty(xml))
-				{
-					xml = "<error>No metadata found for this layer.</error>";
-				}
-			}
-			catch (NotImplementedException ex)
-			{
-				xml = string.Format("<error>{0}</error>", ex.Message);
-			}
+			string xml = GetMetadataXml(ref responseProperties, layerIdInt);
 
 			if (Regex.IsMatch(outputFormat, @"(?i)html?"))
 			{
@@ -149,6 +149,109 @@ namespace LayerMetadataSoe
 				return Encoding.UTF8.GetBytes(xml);
 			}
 
+		}
+
+		private byte[] GetIdsOfLayersThatHaveMetadata(NameValueCollection boundVariables,
+			string outputFormat,
+			string requestProperties,
+			out string responseProperties)
+		{
+			responseProperties = "{\"Content-Type\" : \"application/json\"}";
+			var idArray = GetIdsOfLayersThatHaveMetadata();
+			var serializer = new JavaScriptSerializer();
+			var json = serializer.Serialize(idArray);
+			return Encoding.UTF8.GetBytes(json);
+		}
+
+		private int[] GetIdsOfLayersThatHaveMetadata()
+		{
+			var mapServer = (IMapServer3)_serverObjectHelper.ServerObject;
+			string defaultMapName = mapServer.DefaultMapName;
+
+			IMapServerInfo3 serverInfo = mapServer.GetServerInfo(defaultMapName) as IMapServerInfo3;
+			IMapLayerInfos mapLayerInfos = serverInfo.MapLayerInfos;
+			IMapLayerInfo3 layerInfo = null;
+			var output = new List<int>(mapLayerInfos.Count);
+			for (int i = 0; i < mapLayerInfos.Count; i++)
+			{
+				layerInfo = mapLayerInfos.get_Element(i) as IMapLayerInfo3;
+				if (layerInfo.IsFeatureLayer)
+				{
+					output.Add(i);
+				}
+			}
+
+			return output.ToArray();
+
+
+			////var msDataAccess = (IMapServerDataAccess)_serverObjectHelper.ServerObject;
+			////IDataset layer = null;
+
+			////string xml = null;
+			////responseProperties = "{\"Content-Type\" : \"text/xml\"}";
+			////try
+			////{
+			////    layer = msDataAccess.GetDataSource(defaultMapName, layerId) as IDataset;
+			////}
+		}
+
+		private string GetMetadataXml(ref string responseProperties, int layerId)
+		{
+			var mapServer = (IMapServer3)_serverObjectHelper.ServerObject;
+			string defaultMapName = mapServer.DefaultMapName;
+
+
+
+			var msDataAccess = (IMapServerDataAccess)_serverObjectHelper.ServerObject;
+			IDataset layer = null;
+
+			string xml = null;
+			responseProperties = "{\"Content-Type\" : \"text/xml\"}";
+			try
+			{
+				layer = msDataAccess.GetDataSource(defaultMapName, layerId) as IDataset;
+			}
+#if DEBUG
+			catch (NotImplementedException ex)
+			{
+				var xmlBuilder = new StringBuilder();
+				using (XmlWriter xtw = XmlWriter.Create(xmlBuilder))
+				{
+					xtw.WriteStartDocument();
+					xtw.WriteStartElement("error");
+					xtw.WriteString("Metadata could not be retrieved for this layer.");
+					xtw.WriteStartElement("details");
+					xtw.WriteString(ex.ToString());
+					xtw.WriteEndElement();
+					xtw.WriteEndElement();
+					xtw.WriteEndDocument();
+				}
+				xml = xmlBuilder.ToString();
+			}
+#else
+			catch (NotImplementedException)
+			{
+				xml = string.Format("<error>Metadata could not be retrieved for this layer.</error>");
+			}
+#endif
+
+			if (layer != null)
+			{
+				IMetadata metadata = layer.FullName as IMetadata;
+				if (metadata != null)
+				{
+					IXmlPropertySet2 propSet = metadata.Metadata as IXmlPropertySet2;
+					if (propSet != null)
+					{
+						xml = propSet.GetXml(string.Empty);
+					}
+				}
+			}
+			if (string.IsNullOrEmpty(xml))
+			{
+				xml = "<error>No metadata found for this layer.</error>";
+			}
+			return xml;
 		}
 
 		/// <summary>
